f6aff06fc04205adf36ac736b00e516fa482cbe2
big-green-lemon <bWl0c3VoYQ@protonmail.com>
2026-01-03 17:45:29 +0100

    Repaired tests

--- tests/perft.rs
+++ tests/perft.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod tests {
-    use chess_engine::engine::models::board::Chessboard;
+    use lib::{engine::models::board::Chessboard, perft};
 
     // Note this useful idiom: importing names from outer (for mod tests) scope.
     use super::*;
@@ -9,11 +9,11 @@ mod tests {
     fn test_perft_1() {
         let fen = "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1";
         let mut chessboard = Chessboard::from_fen(fen).unwrap();
-        assert_eq!(perft(chessboard, 1), 20);
-        assert_eq!(dasfasfasfasf(chessboard, 2), 400);
-        assert_eq!(perft(chessboard, 3), 8902);
-        assert_eq!(perft(chessboard, 4), 197281);
-        assert_eq!(perft(chessboard, 5), 4865609);
+        assert_eq!(perft(&mut chessboard, 1), 20);
+        assert_eq!(perft(&mut chessboard, 2), 400);
+        assert_eq!(perft(&mut chessboard, 3), 8902);
+        assert_eq!(perft(&mut chessboard, 4), 197281);
+        assert_eq!(perft(&mut chessboard, 5), 4865609);
     }
 
     #[test]
@@ -21,9 +21,9 @@ mod tests {
         let fen = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R w KQkq - 0 1";
         let mut chessboard = Chessboard::from_fen(fen).unwrap();
 
-        assert_eq!(perft(chessboard, 1), 48);
-        assert_eq!(perft(chessboard, 2), 2039);
-        assert_eq!(perft(chessboard, 3), 97862);
+        assert_eq!(perft(&mut chessboard, 1), 48);
+        assert_eq!(perft(&mut chessboard, 2), 2039);
+        assert_eq!(perft(&mut chessboard, 3), 97862);
     }
 
     #[test]
@@ -31,11 +31,11 @@ mod tests {
         let fen = "8/2p5/3p4/KP5r/1R3p1k/8/4P1P1/8 w - - 0 1";
         let mut chessboard = Chessboard::from_fen(fen).unwrap();
 
-        assert_eq!(perft(chessboard, 1), 14);
-        assert_eq!(perft(chessboard, 2), 191);
-        assert_eq!(perft(chessboard, 3), 2812);
-        assert_eq!(perft(chessboard, 4), 43238);
-        assert_eq!(perft(chessboard, 5), 674624);
+        assert_eq!(perft(&mut chessboard, 1), 14);
+        assert_eq!(perft(&mut chessboard, 2), 191);
+        assert_eq!(perft(&mut chessboard, 3), 2812);
+        assert_eq!(perft(&mut chessboard, 4), 43238);
+        assert_eq!(perft(&mut chessboard, 5), 674624);
     }
 
     #[test]
@@ -43,9 +43,9 @@ mod tests {
         let fen = "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P2PP/R2Q1RK1 w kq - 0 1";
         let mut chessboard = Chessboard::from_fen(fen).unwrap();
 
-        assert_eq!(perft(chessboard, 1), 6);
-        assert_eq!(perft(chessboard, 2), 264);
-        assert_eq!(perft(chessboard, 3), 9467);
+        assert_eq!(perft(&mut chessboard, 1), 6);
+        assert_eq!(perft(&mut chessboard, 2), 264);
+        assert_eq!(perft(&mut chessboard, 3), 9467);
     }
 
     #[test]
@@ -53,9 +53,9 @@ mod tests {
         let fen = "rnbq1k1r/pp1Pbppp/2p5/8/2B5/8/PPP1NnPP/RNBQK2R w KQ - 1 8";
         let mut chessboard = Chessboard::from_fen(fen).unwrap();
 
-        assert_eq!(perft(chessboard, 1), 44);
-        assert_eq!(perft(chessboard, 2), 1486);
-        assert_eq!(perft(chessboard, 3), 62379);
+        assert_eq!(perft(&mut chessboard, 1), 44);
+        assert_eq!(perft(&mut chessboard, 2), 1486);
+        assert_eq!(perft(&mut chessboard, 3), 62379);
     }
 
     #[test]
@@ -63,9 +63,9 @@ mod tests {
         let fen = "r4rk1/1pp1qppp/p1np1n2/2b1p1B1/2B1P1b1/P1NP1N2/1PP1QPPP/R4RK1 w - - 0 10";
         let mut chessboard = Chessboard::from_fen(fen).unwrap();
 
-        assert_eq!(perft(chessboard, 1), 46);
-        assert_eq!(perft(chessboard, 2), 2079);
-        assert_eq!(perft(chessboard, 3), 89890);
+        assert_eq!(perft(&mut chessboard, 1), 46);
+        assert_eq!(perft(&mut chessboard, 2), 2079);
+        assert_eq!(perft(&mut chessboard, 3), 89890);
     }
 
     #[test]
@@ -73,8 +73,8 @@ mod tests {
         let fen = "k7/8/8/8/p7/8/7P/7K w - - 1 1";
         let mut chessboard = Chessboard::from_fen(fen).unwrap();
 
-        assert_eq!(perft(chessboard, 1), 4);
-        assert_eq!(perft(chessboard, 2), 16);
+        assert_eq!(perft(&mut chessboard, 1), 4);
+        assert_eq!(perft(&mut chessboard, 2), 16);
     }
 
     #[test]
@@ -82,7 +82,7 @@ mod tests {
         let fen = "k7/8/8/8/p6P/8/8/7K b - H3 1 1";
         let mut chessboard = Chessboard::from_fen(fen).unwrap();
 
-        assert_eq!(perft(chessboard, 2), 16);
+        assert_eq!(perft(&mut chessboard, 2), 16);
     }
 
     #[test]
@@ -90,8 +90,8 @@ mod tests {
         let fen = "k7/8/8/8/p7/8/7P/7K b - - 1 1";
         let mut chessboard = Chessboard::from_fen(fen).unwrap();
 
-        assert_eq!(perft(chessboard, 1), 4);
-        assert_eq!(perft(chessboard, 2), 16);
+        assert_eq!(perft(&mut chessboard, 1), 4);
+        assert_eq!(perft(&mut chessboard, 2), 16);
     }
 
     #[test]
@@ -99,7 +99,7 @@ mod tests {
         let fen = "r3k2r/p6p/P6P/8/8/p6p/P6P/R3K2R w KQkq - 0 1";
         let mut chessboard = Chessboard::from_fen(fen).unwrap();
 
-        assert_eq!(perft(chessboard, 1), 12);
+        assert_eq!(perft(&mut chessboard, 1), 12);
     }
 
     #[test]
@@ -107,7 +107,7 @@ mod tests {
         let fen = "r3k2r/p1ppqpb1/bn2pnp1/3PN3/1p2P3/2N2Q1p/PPPBBPPP/R3K2R b KQkq - 0 1";
         let mut chessboard = Chessboard::from_fen(fen).unwrap();
 
-        assert_eq!(perft(chessboard, 1), 43);
+        assert_eq!(perft(&mut chessboard, 1), 43);
     }
 
     #[test]
@@ -115,7 +115,7 @@ mod tests {
         let fen = "8/2p5/3p4/KP5r/1R3pPk/8/4P3/8 b - g3 0 1";
         let mut chessboard = Chessboard::from_fen(fen).unwrap();
 
-        assert_eq!(perft(chessboard, 1), 17);
+        assert_eq!(perft(&mut chessboard, 1), 17);
     }
 
     #[test]
@@ -123,7 +123,7 @@ mod tests {
         let fen = "r3k2r/p6p/P6P/8/8/8/8/4K3 b kq - 0 1";
         let mut chessboard = Chessboard::from_fen(fen).unwrap();
 
-        assert_eq!(perft(chessboard, 1), 12);
+        assert_eq!(perft(&mut chessboard, 1), 12);
     }
 
     #[test]
@@ -131,7 +131,7 @@ mod tests {
         let fen = "5k2/7R/5K1P/5P2/8/8/8/8 w - - 0 1";
         let mut chessboard = Chessboard::from_fen(fen).unwrap();
 
-        assert_eq!(perft(chessboard, 2), 20);
+        assert_eq!(perft(&mut chessboard, 2), 20);
     }
 
     #[test]
@@ -139,8 +139,8 @@ mod tests {
         let fen = "r3k2r/Pppp1ppp/1b3nbN/nP6/BBP1P3/q4N2/Pp1P1RPP/R2Q2K1 b kq - 0 1";
         let mut chessboard = Chessboard::from_fen(fen).unwrap();
 
-        assert_eq!(perft(chessboard, 1), 45);
-        assert_eq!(perft(chessboard, 2), 1623);
+        assert_eq!(perft(&mut chessboard, 1), 45);
+        assert_eq!(perft(&mut chessboard, 2), 1623);
     }
 
     #[test]
@@ -148,8 +148,8 @@ mod tests {
         let fen = "rnbq1k1r/pp1Pbppp/2p5/8/2B5/P7/1PP1NnPP/RNBQK2R b KQ - 1 8";
         let mut chessboard = Chessboard::from_fen(fen).unwrap();
 
-        assert_eq!(perft(chessboard, 1), 34);
-        assert_eq!(perft(chessboard, 2), 1373);
+        assert_eq!(perft(&mut chessboard, 1), 34);
+        assert_eq!(perft(&mut chessboard, 2), 1373);
     }
 
     #[test]
@@ -157,7 +157,7 @@ mod tests {
         let fen = "K6k/8/6Q1/8/8/8/3p4/8 b - - 0 1";
         let mut chessboard = Chessboard::from_fen(fen).unwrap();
 
-        assert_eq!(perft(chessboard, 1), 4);
+        assert_eq!(perft(&mut chessboard, 1), 4);
     }
 
     #[test]
@@ -165,7 +165,7 @@ mod tests {
         let fen = "7k/8/8/8/1p6/7p/P5P1/R3K3 w Q - 0 1";
         let mut chessboard = Chessboard::from_fen(fen).unwrap();
 
-        assert_eq!(perft(chessboard, 4), 10087);
+        assert_eq!(perft(&mut chessboard, 4), 10087);
     }
 
     #[test]
@@ -173,6 +173,6 @@ mod tests {
         let fen = "8/1p3p1p/5PkP/5pPp/P4PpP/5pKp/5P1P/8 b - - 0 2";
         let mut chessboard = Chessboard::from_fen(fen).unwrap();
 
-        assert_eq!(perft(chessboard, 2), 3);
+        assert_eq!(perft(&mut chessboard, 2), 3);
     }
 }
